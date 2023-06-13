bec4213c58392a91dffcc1f7a286c49d650f6ea4
Faiaz Rahman <faiazrahman70@gmail.com>
2023-06-13 20:50:48 +0600

    Filter elements Created

---
+++ src/component/Filters.js
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { COLORS, DIM } from '../asset/theme'
+
+export default function Filters({ title }) {
+  return (
+    <TouchableOpacity style={styles.filterContainer}>
+      <Text style={styles.filterText}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  filterContainer: {
+    width: DIM.width * 0.2,
+    height: '90%',
+    // backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    borderWidth: 4,
+    borderColor: COLORS.boxOutline,
+  },
+  filterText: {
+    color: 'black',
+    fontWeight: '800',
+    fontSize: 17,
+  },
+})
